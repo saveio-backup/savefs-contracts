ca10f7ed0658cfa84d827e67c29323a184932d83
smallyunet <dev.wangyu@gmail.com>
2022-09-21 12:00:56 +0800

    delete deploy script
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,11 +1,11 @@
 const { ethers } = require("hardhat");
 
 const url = "http://127.0.0.1:8545";
-const address = "0xd369b085e8907cBa039dD2BFA7b32E31a43273D3";
+const address = "0x0bd81846A232E263bC289A0B2206Ae4ae26A08A2";
 const privateKey =
-  "07c1ba70f06ffb7752d0cf6edb150148ed5113d425ad5bec6d820ab12197d486";
+  "5e2434f0a266601bc7b03313dfada0d6bb7706aa78723c01df0e7bbf92eb804e";
 
-let nodeAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+let nodeAddress = "0x0bd81846A232E263bC289A0B2206Ae4ae26A08A2";
 
 let provider = new ethers.providers.JsonRpcProvider(url);
 let signer = new ethers.Wallet(privateKey, provider);
